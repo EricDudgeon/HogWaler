1396b9982965b4fd9c92ac2f1f2cc26d9fcb3bed
Albert <bertsky@ksu.edu>
2022-02-26 21:15:13 -0600

    test2

--- server.js
+++ server.js
@@ -4,12 +4,10 @@ const app = require('./src/app');
 //require('./src/database');
 require('./src/templates');
 const port = 8084;
-//test
 const reload = require('reload');
 
 app.listen(port, function(){
     console.log("Server is listening on port " + port);
 });
-//test1
 
 reload(app);
